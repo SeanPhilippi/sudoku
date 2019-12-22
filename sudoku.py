39baf6c54369bb31756f6d7b671b960f6b61f362
Sean Philippi <sean.philippi@gmail.com>
2019-12-22 15:38:43 -0600

    more notes

--- sudoku.py
+++ sudoku.py
@@ -11,17 +11,22 @@ board = [
 ]
 
 def print_board(board):
-  for i in range(len(board)):
+  # range returns a sequence of #s from 0 to n
+  for i in range(len(board)): # for item in [0-8]
+    #print('i', i)
+    # determine if a line should be printed based on row #
     if i % 3 == 0 and i != 0:
-      print('- - - - - - - - - - - - ')
-
-    for j in range(len(board[0])):
+      print('- - - - - - - - - - - -')
+    # logic for what characters to print on each row
+    for j in range(len(board)):
       if j % 3 == 0 and j != 0:
         print(' | ', end='') # setting print end argument to empty string instead of deafult \n
 
       if j == 8:
+        # print last character with the default end argument \n
         print(board[i][j])
       else:
+        # print the character at board[i][j] with end argument of '' so more characters concatenate
         print(str(board[i][j]) + ' ', end='')
 
 print_board(board)
