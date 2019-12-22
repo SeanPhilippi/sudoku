e1cd2642902251c2792555c95d3a6854f7212534
Sean Philippi <sean.philippi@gmail.com>
2019-12-22 15:23:24 -0600

    board looking beautiful

--- sudoku.py
+++ sudoku.py
@@ -13,12 +13,13 @@ board = [
 def print_board(board):
   for i in range(len(board)):
     if i % 3 == 0 and i != 0:
-      print('\n- - - - - - - - - - - - - ')
+      print('- - - - - - - - - - - - ')
+
     for j in range(len(board[0])):
       if j % 3 == 0 and j != 0:
-        print(' | ', end='')
+        print(' | ', end='') # setting print end argument to empty string instead of deafult \n
 
-      if j == 0:
+      if j == 8:
         print(board[i][j])
       else:
         print(str(board[i][j]) + ' ', end='')
