d17e97c330342d6e69a6156cc761f8a06bd3e43e
Sean Philippi <sean.philippi@gmail.com>
2019-12-26 18:00:24 -0600

    valid function row checking with notes, col checking started

--- sudoku.py
+++ sudoku.py
@@ -10,6 +10,33 @@ board = [
   [5,4,6,1,7,7,6,9,0]
 ]
 
+# the board (array of arrays), num being inserted, and coordinates for insertion
+def valid(board, num, coords):
+  # check row
+  # for each position in range of 9
+  for i in range(len(board[0])):
+    # looping 0-8, checking the number at each coordinate for coords[0] row, if it is equal to
+    # the insertion # and the coords[1] col number is not equal to the column number, return False
+    # so ignore the number if it's the number that was just inserted
+    if board[coords[0]][i] == num and coords[1] != i:
+      return False
+
+  # check column
+  for i in range(len(board)):
+    if board[i][coords[1]] == num and coords[0] != i:
+      return False
+
+  # check box
+  box_x = coords[1] // 3
+  box_y = coords[0] // 3
+
+  for i in range(box_y * 3, box_y * 3 + 3):
+    for j in range(box_x * 3, box_x * 3 + 3):
+      if board[i][j] == num and (i, j) != coords:
+        return False
+
+valid(board, 1, (1, 2))
+
 def print_board(board):
   # range returns a sequence of #s from 0 to n
   for i in range(len(board)): # for item in [0-8]
