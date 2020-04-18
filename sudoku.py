777f8af6a2b68c050c265677ce5d3dd3c4b71806
SeanPhilippi <sean.philippi@gmail.com>
2020-04-18 17:49:53 -0400

    test push from linux

--- sudoku.py
+++ sudoku.py
@@ -40,7 +40,7 @@ valid(board, 1, (1, 2))
 def print_board(board):
   # range returns a sequence of #s from 0 to n
   for i in range(len(board)): # for item in [0-8]
-    #print('i', i)
+    # print('i', i)
     # determine if a line should be printed based on row #
     if i % 3 == 0 and i != 0:
       print('- - - - - - - - - - - -')
@@ -63,6 +63,6 @@ def find_empty(board):
   for i in range(len(board)): # for each row
     for j in range(len(board[0])): # for each column
       if board[i][j] == 0:
-        return (i, j) # return tuple of row, column
+        return (i, j) # return tuple of row (y), column (x)
 
 print(find_empty(board))
